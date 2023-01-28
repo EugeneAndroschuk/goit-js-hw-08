c05df0c28b1874168c7f64ec8801b29783f0896e
EugeneAndroschuk <enet2611@gmail.com>
2023-01-28 16:30:36 +0200

    player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Vimeo(iframe);
+const savedTime = load('videoplayer-current-time');
+
+player.setCurrentTime(savedTime);
+
+player.on('timeupdate', throttle((data) => onPlay(data), 1000));
+
+function onPlay(data) {
+  const currentTime = data.seconds;
+  save('videoplayer-current-time', currentTime);
+};
+
+function save(key, value) {
+  try {
+    localStorage.setItem(key, value);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function load(key) {
+  try {
+    const time = localStorage.getItem(key);
+    return time === null ? 0.000 : time;
+  } catch (err) {
+    console.error(err);
+  }
+}
